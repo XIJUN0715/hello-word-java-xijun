14a4533cd2b3f5d7124e7f491a58a540e2ba4268
xijun <xijunlee15@utar.my>
2026-04-09 13:56:36 +0800

    2nd commit here

--- src/test/java/com/example/HelloWorldTest.java
+++ src/test/java/com/example/HelloWorldTest.java
@@ -6,6 +6,6 @@ public class HelloWorldTest {
     @Test
     public void testGetGreeting() {
         HelloWorld helloWorld = new HelloWorld();
-        assertEquals("Hello, World!", helloWorld.getGreeting());
+        assertEquals("Hello, World!", helloWorld.getGreeting()); //there are two different version if the assert equals compare to the Hello words there
     }
 }
